0ec1f71de581774cbaf9c3046e216a19e20d5ba2
Sergey <orangedao86@gmail.com>
2017-08-20 15:14:35 +0300

    add new code

--- parse_img.py
+++ parse_img.py
@@ -2,33 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import os
 
 urls = [
-    'https://apse2-uploads5.wikiart.org/images/salvador-dali.jpg!Portrait.jpg'
-    'https://apse2-uploads7.wikiart.org/images/salvador-dali/un-chien-andalou-film-still-1928.jpg!PinterestLarge.jpg'
-    'https://apse2-uploads1.wikiart.org/images/salvador-dali/the-great-masturbator-1929.jpg!PinterestLarge.jpg'
-    'https://apse2-uploads5.wikiart.org/images/salvador-dali/the-persistence-of-memory-1931.jpg!PinterestLarge.jpg'
-]
-
+    'https://d32dm0rphc51dk.cloudfront.net/Rn549IGXSwy20o5rK2FWrw/larger.jpg'
+    ]
 
 def get_file(url):
     r = requests.get(url, stream=True)
     return r
 
-def get_name(url):
-	name = url.split('/')[-1]
-	return name
+def get_name(url):    	    
+    name = url.split('/')[-1]
+    folder = name.split('.')[0]
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    path = os.path.abspath(folder)
+
+    return path + '/' + name
+
 
 def save_image(name, file_object):
-	with open(name, 'bw') as f:
-		for chunk in file_object.iter_content(8192):
-			f.write(chunk)
+    with open(name, 'bw') as f:
+        for chunk in file_object.iter_content(8192):
+            f.write(chunk)
 
 
 def main():
 
-	for url in urls:
-		save_image(get_name(url), get_file(url))
+    for url in urls:
+        save_image(get_name(url), get_file(url))
 
 
 if __name__ == '__main__':
