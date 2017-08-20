c355b2db92e871ebe5a5fa238d0b0c293c777fb2
Sergey <orangedao86@gmail.com>
2017-08-20 13:03:27 +0300

    add new rule

--- test_parse.py
+++ test_parse.py
@@ -8,18 +8,28 @@ import os
 
 
 # url = 'http://www.blog.pythonlibrary.org/wp-content/uploads/2012/06/wxDbViewer.zip'
-url1 = 'https://uploads8.wikiart.org/images/philippe-halsman/garters-1939.jpg'
-p = os.path.dirname(url1)
-name_part1 = p.split(sep='/')[-1]
-name_part2 = os.path.basename(url1)
-file_name = name_part1 + '-' + name_part2
-print(file_name)
+# url = 'https://uploads8.wikiart.org/images/philippe-halsman/garters-1939.jpg'
+# p = os.path.dirname(url)
+# name_part1 = p.split(sep='/')[-1]
+# name_part2 = os.path.basename(url)
+# file_name = name_part1 + '-' + name_part2
+# print(file_name)
 
-
-
+url = 'https://uploads4.wikiart.org/images/salvador-dali/flower-in-the-desert.jpg!PinterestLarge.jpg'
+url_img = url.split(sep='!PinterestLarge.jpg')[0]
+print(url_img)
 
 # print("downloading with requests")
-# r = requests.get(url1)
+# r = requests.get(url_img)
 # # with open("code3.zip", "wb") as code:
 # with open("001.jpg", "wb") as img:
 #     img.write(r.content)
+https://apse2-uploads5.wikiart.org/images/salvador-dali.jpg!Portrait.jpg
+https://apse2-uploads7.wikiart.org/images/salvador-dali/un-chien-andalou-film-still-1928.jpg!PinterestLarge.jpg
+https://apse2-uploads1.wikiart.org/images/salvador-dali/the-great-masturbator-1929.jpg!PinterestLarge.jpg
+https://apse2-uploads5.wikiart.org/images/salvador-dali/the-persistence-of-memory-1931.jpg!PinterestLarge.jpg
+https://apse2-uploads5.wikiart.org/images/salvador-dali/dream-caused-by-the-flight-of-a-bee-around-a-pomegranate-one-second-before-awakening.jpg!PinterestLarge.jpg
+https://apse2-uploads8.wikiart.org/images/salvador-dali/young-virgin-auto-sodomized-by-the-horns-of-her-own-chastity.jpg!PinterestLarge.jpg
+https://apse2-uploads4.wikiart.org/images/salvador-dali/the-disintegration-of-the-persistence-of-memory.jpg!PinterestLarge.jpg
+https://apse2-uploads1.wikiart.org/images/salvador-dali/the-discovery-of-america-by-christopher-columbus-1959.jpg!PinterestLarge.jpg
+https://apse2-uploads3.wikiart.org/images/salvador-dali/hallucinogenic-toreador-1970.jpg!PinterestLarge.jpg
