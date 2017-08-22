f7f55df0be8a8688ac38795abcc22926fb77b227
Sergey <orangedao86@gmail.com>
2017-08-22 22:48:48 +0300

    add new code. сохранение картинок из списка urls

--- parse_img.py
+++ parse_img.py
@@ -1,25 +1,38 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests
 import os
+import requests
 
 urls = [
-    'https://d32dm0rphc51dk.cloudfront.net/Rn549IGXSwy20o5rK2FWrw/larger.jpg'
-    ]
+    'https://uploads6.wikiart.org/images/salvador-dali/car-clothing-clothed-automobile.jpg!PinterestLarge.jpg',
+    'https://uploads4.wikiart.org/images/salvador-dali/costume-for-a-nude-with-a-codfish-tail.jpg!PinterestLarge.jpg',
+    'https://uploads4.wikiart.org/images/salvador-dali/design-for-set-curtain-for-labyrinth-i.jpg!PinterestLarge.jpg',
+    'https://uploads1.wikiart.org/images/salvador-dali/invisible-bust-of-voltaire.jpg!PinterestLarge.jpg'
+]
+
+folder = 'images'
+
+
+def get_large_file(url):
+    url = url.split('!')[0]
+    return url
+
 
 def get_file(url):
     r = requests.get(url, stream=True)
     return r
 
-def get_name(url):    	    
-    name = url.split('/')[-1]
-    folder = name.split('.')[0]
+
+def get_name(url):
+    name = url.split('/')[-1].split('!')[-2]
+    # folder = name.split('.')[0]
 
     if not os.path.exists(folder):
         os.makedirs(folder)
-    
+
     path = os.path.abspath(folder)
+    print(name)
 
     return path + '/' + name
 
@@ -31,9 +44,8 @@ def save_image(name, file_object):
 
 
 def main():
-
     for url in urls:
-        save_image(get_name(url), get_file(url))
+        save_image(get_name(url), get_file(get_large_file(url)))
 
 
 if __name__ == '__main__':
