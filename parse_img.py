2d5a4add45d250efd590530d5a828f7ba65cb379
Sergey <orangedao86@gmail.com>
2017-08-31 01:44:11 +0300

    добавлена ф-я с мультипроцесным режимом. небольшая доработка других ф-й

--- parse_img.py
+++ parse_img.py
@@ -3,20 +3,21 @@
 
 import os
 import requests
+from multiprocessing import Pool
 '''
 Для работы с программой необходимо подготовить входные данные.
 Для этого помещаем список ссылок во входной каталог.
 
-Программа работает в однопоточном режиме
+Программа работает в многопроцессорном режиме
 '''
 # ----Переменные-----
-folder_out = 'images'  # переменная каталога с картинками
-folder_in = 'files_in'  # переменная каталога с входящими данными
-file_data = 'url_img.txt'  # переменная для рабочего файла с урлами
-path_file_in = os.path.join(folder_in, file_data)
-msg_url = 'Данные отсутствуют.\
-        \nСохраните список урлов в файле <{}>.\
-        \nКаждый урл должен находиться на новой строке.'.format(path_file_in)
+# folder_out = 'images'  # переменная каталога с картинками
+# folder_in = 'files_in'  # переменная каталога с входящими данными
+# file_data = 'url_img.txt'  # переменная для рабочего файла с урлами
+# path_file_in = os.path.join(folder_in, file_data)
+# msg_url = 'Данные отсутствуют.\
+#         \nСохраните список урлов в файле <{}>.\
+#         \nКаждый урл должен находиться на новой строке.'.format(path_file_in)
 
 
 # функция чтения файлов из каталога и получения списка ссылок
@@ -43,11 +44,11 @@ def get_file(url):
 # получения имени большой картинки
 def get_name(url):
     name = url.split('/')[-1].split('!')[-2]
-    if not os.path.exists(folder_out):
-        os.makedirs(folder_out)
-    path = os.path.abspath(folder_out)
+    # if not os.path.exists(folder_out):
+    #     os.mkdir(folder_out)
+    path = os.path.join(folder_out, name)
     print(name)
-    return path + '/' + name
+    return path
 
 
 # ф-ф сохранения картинки в каталог
@@ -58,21 +59,36 @@ def save_image(name, file_object):
 
 
 # ф-я создания входного каталога и пустого файла для сохранения урлов
-def create_path_in(path_file_in, folder_in):
+def create_work_dirs(path_file_in, folder_in, folder_out):
     if not os.path.exists(folder_in):
         os.mkdir(folder_in)
-        print('Каталог <{}> создан.'.format(folder_in))
+        # print('Каталог <{}> создан.'.format(folder_in))
     if not os.path.exists(path_file_in):
         with open(path_file_in, 'w') as f:
             f.write('')
+    if not os.path.exists(folder_out):
+        os.mkdir(folder_out)
+
+
+def make_all(url):
+    save_image(get_name(url), get_file(get_large_file(url)))
+    # pass
 
 
 def main():
-    create_path_in(path_file_in, folder_in)
+    create_work_dirs(path_file_in, folder_in, folder_out)
     urls = read_file(path_file_in)
-    for url in urls:
-        save_image(get_name(url), get_file(get_large_file(url)))
+    with Pool(10) as p:
+        p.map(make_all, urls)
 
 
 if __name__ == '__main__':
+    folder_out = 'images'  # переменная каталога с картинками
+    folder_in = 'files_in'  # переменная каталога с входящими данными
+    file_data = 'url_img.txt'  # переменная для рабочего файла с урлами
+    path_file_in = os.path.join(folder_in, file_data)
+    msg_url = 'Данные отсутствуют.\
+            \nСохраните список урлов в файле <{}>.\
+            \nКаждый урл должен находиться на новой строке.'.format(path_file_in)
+
     main()
