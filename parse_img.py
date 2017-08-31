19fc1975e4941a94969a3cb927b375c65b85f5a4
Sergey <orangedao86@gmail.com>
2017-08-31 12:04:38 +0300

    небольшие изменения в коде

--- parse_img.py
+++ parse_img.py
@@ -10,14 +10,6 @@ from multiprocessing import Pool
 
 Программа работает в многопроцессорном режиме
 '''
-# ----Переменные-----
-# folder_out = 'images'  # переменная каталога с картинками
-# folder_in = 'files_in'  # переменная каталога с входящими данными
-# file_data = 'url_img.txt'  # переменная для рабочего файла с урлами
-# path_file_in = os.path.join(folder_in, file_data)
-# msg_url = 'Данные отсутствуют.\
-#         \nСохраните список урлов в файле <{}>.\
-#         \nКаждый урл должен находиться на новой строке.'.format(path_file_in)
 
 
 # функция чтения файлов из каталога и получения списка ссылок
@@ -44,10 +36,7 @@ def get_file(url):
 # получения имени большой картинки
 def get_name(url):
     name = url.split('/')[-1].split('!')[-2]
-    # if not os.path.exists(folder_out):
-    #     os.mkdir(folder_out)
     path = os.path.join(folder_out, name)
-    print(name)
     return path
 
 
@@ -56,13 +45,13 @@ def save_image(name, file_object):
     with open(name, 'bw') as f:
         for chunk in file_object.iter_content(8192):
             f.write(chunk)
+    print('Файл <{}> сохранен'.format(name))
 
 
-# ф-я создания входного каталога и пустого файла для сохранения урлов
+# ф-я создания входного и выходного каталогов и пустого файла для сохранения урлов
 def create_work_dirs(path_file_in, folder_in, folder_out):
     if not os.path.exists(folder_in):
         os.mkdir(folder_in)
-        # print('Каталог <{}> создан.'.format(folder_in))
     if not os.path.exists(path_file_in):
         with open(path_file_in, 'w') as f:
             f.write('')
@@ -70,20 +59,24 @@ def create_work_dirs(path_file_in, folder_in, folder_out):
         os.mkdir(folder_out)
 
 
+# вспомогательная ф-я для запуска в  многопроцесорном режиме
 def make_all(url):
     save_image(get_name(url), get_file(get_large_file(url)))
-    # pass
 
 
 def main():
     create_work_dirs(path_file_in, folder_in, folder_out)
     urls = read_file(path_file_in)
-    with Pool(10) as p:
+    number_proc = 5  # кол-во процессов
+    with Pool(number_proc) as p:
         p.map(make_all, urls)
+    print('DONE!')
 
 
 if __name__ == '__main__':
     folder_out = 'images'  # переменная каталога с картинками
+    # folder_out = input('Введите название каталога: ')
+
     folder_in = 'files_in'  # переменная каталога с входящими данными
     file_data = 'url_img.txt'  # переменная для рабочего файла с урлами
     path_file_in = os.path.join(folder_in, file_data)
